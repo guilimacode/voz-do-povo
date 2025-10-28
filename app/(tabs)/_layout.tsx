f39357bd31b232a32af2c2fd8ee7abedf5af8093
DanielSSPP <danielsamp302@gmail.com>
2025-10-27 22:11:17 -0300

    Telas de publicação criadas

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,9 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+
+import { Colors } from 'constants/theme';
+import { useColorScheme } from 'hooks/use-color-scheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
